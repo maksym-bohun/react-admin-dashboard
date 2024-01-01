5cace7430572aaceb7de27099466f73ee9eac2f5
Maksym-Bohun <maks.boggun@gmail.com>
2024-01-01 20:48:03 +0200

    created menu design

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,6 +1,24 @@
+import { Link } from "react-router-dom";
+import { menu } from "../../data";
 import "./menu.scss";
 const Menu = () => {
-  return <div className="menu">Menu</div>;
+  return (
+    <div className="menu">
+      {menu.map((item) => {
+        return (
+          <div className="item" key={item.id}>
+            <span className="title">{item.title}</span>
+            {item.listItems.map((listItem) => (
+              <Link key={listItem.id} className="listItem" to={listItem.url}>
+                <img src={listItem.icon} alt="icon" />
+                <span className="listItemTitle">{listItem.title}</span>
+              </Link>
+            ))}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Menu;
